9a9430d0a49cb5e3fc7cb2461e841bde675ef737
mikaOg <thomasn4jackson08@gmail.com>
2026-09-11 08:37:10 +0300

    Update Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -5,11 +5,11 @@ export default function Logo({ light = false }: { light?: boolean }) {
   return (
     <Link href="/" className="group flex items-center">
       <Image
-        src="/logo.png"
+        src="/tours/logo.png"
         alt="Ethio Connect to Desert"
         width={80}
         height={80}
-        className="h-12 w-auto object-contain transition-transform duration-300 group-hover:scale-105 sm:h-14"
+        className="h-11 w-auto object-contain transition-transform duration-300 group-hover:scale-105 sm:h-12"
         priority
       />
     </Link>
